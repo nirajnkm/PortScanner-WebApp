9dc751ae9f9bd33a7e3c0aecac838fa630e0392b
Niraj Kumar Mahto <nirajmahto72@gmail.com>
2024-06-01 11:59:38 +0530

    update

--- scan.py
+++ scan.py
@@ -3,71 +3,49 @@
 from argparse import ArgumentParser
 import socket
 from threading import Thread
-from time import time
-import subprocess
-
-
-open_ports = []
-
 
 def prepare_args():
+    parser = ArgumentParser(description="PythonBased Fast Port Scanner", usage="%(prog)s -s 20 -e 40000 -t 500 -V 192.168.1.1")        
+    parser.add_argument(metavar="IPv4", dest="ip", help="host to scan")
+    parser.add_argument("-s", "--start", dest="start", metavar="\b", type=int, help="starting port", default=1)
+    parser.add_argument("-e", "--end", dest="end", metavar="\b", type=int, help="end port", default=65535)
+    parser.add_argument("-t", "--threads", dest="threads", metavar="\b", type=int, help="number of threads", default=500)
+    parser.add_argument("-V", "--verbose", dest="verbose", action="store_true", help="verbose")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.0")
+    args = parser.parse_args()
+    return args
 
-	parser = ArgumentParser(description="PythonBased Fast Port Scanner",usage="%(prog)s -s 20 -e 40000 -t 500 -V 192.168.1.1")       	
-	parser.add_argument(metavar="IPv4",dest="ip",help="host to scan")
-	parser.add_argument("-s","--start",dest="start",metavar="\b",type=int,help="starting port",default=1)
+def prepare_ports(start:int, end:int):
+    for port in range(start, end+1):
+        yield port
 
-	parser.add_argument("-e","--end",dest="end",metavar="\b",type=int,help="end port",default=65535)
+def scan_port(ip, port, verbose, open_ports):
+    try:
+        s = socket.socket()
+        s.settimeout(1)
+        s.connect((ip, port))
+        open_ports.append(port)
+    except (ConnectionRefusedError, socket.timeout):
+        pass
 
-	parser.add_argument("-t","--threads",dest="threads",metavar="\b",type=int,help="number of threads",default=500)
+def prepare_threads(ip, ports, threads:int, verbose):
+    open_ports = []
+    thread_list = []
+    for port in ports:
+        thread = Thread(target=scan_port, args=(ip, port, verbose, open_ports))
+        thread_list.append(thread)
+    for thread in thread_list:
+        thread.start()
+    for thread in thread_list:
+        thread.join()
+    return open_ports
 
-	parser.add_argument("-V","--verbose",dest="verbose",action="store_true",help="vervose")
-	parser.add_argument("-v","--version",action="version",version="%(prog)s 1.0")
-	args = parser.parse_args()
-	return args
-
-def prepare_ports(start:int,end:int):
-	for port in range(start,end+1):
-                yield port
-				
-def scan_port():
-        
-        while True:
-                
-                try:
-                        s=socket.socket()
-                        s.settimeout(1)
-                        port = next(ports)
-                        s.connect((arguments.ip,port))
-                        open_ports.append(port)
-                        if arguments.verbose:
-                                print(f"\r{open_ports}",end="")
-                except (ConnectionRefusedError,socket.timeout):
-                        continue
-                except StopIteration:
-                        break                
-
-
-def prepare_threads(threads:int):
-        thread_list = []
-        for _ in range(threads):
-                thread_list.append(Thread(target=scan_port))    
-        for thread in thread_list:
-                thread.start()  
-        for thread in thread_list:
-                thread.join()
-		  			
+def get_open_ports(ip, start, end, threads, verbose):
+    ports = prepare_ports(start, end)
+    open_ports = prepare_threads(ip, ports, threads, verbose)
+    return open_ports
 
 if __name__ == "__main__":
-        arguments = prepare_args()
-        ports = prepare_ports(arguments.start,arguments.end)
-        #start_time = time()
-        prepare_threads(arguments.threads)
-        #end_time = time()
-        #if arguments.verbose:
-        #       print()
-        #print(f"Open Ports Found - {open_ports}")
-        #print(f"Time taken {round(end_time - start_time)}")
-        with open('openports.txt', 'w') as file:
-        # Write Python code to the file
-                file.write(str(open_ports))
-
+    arguments = prepare_args()
+    open_ports = get_open_ports(arguments.ip, arguments.start, arguments.end, arguments.threads, arguments.verbose)
+    print(f"Open Ports Found: {open_ports}")
